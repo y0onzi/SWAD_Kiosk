1dfecd1846edd99b44d81894cb1bc08162174e5d
haujin99 <80439082+haujin99@users.noreply.github.com>
2023-06-04 01:34:58 +0900

    Update Inventory.java
    get 부분 통일

--- src/Inventory.java
+++ src/Inventory.java
@@ -29,7 +29,7 @@ public class Inventory {
     boolean inventoryStateofSprite = true;
 
 
-    public static int getIng(int k) {
+    public static int getInv(int k) {
         if(k == 0)
             return numOfKrabbyPatty;
         else if(k == 1)
@@ -50,60 +50,46 @@ public class Inventory {
             return amountOfSprite;
     }
 
-    public int getKrabbyPatty() {
-        return KrabbyPatty;
-    }
-    public int getBulgogiBurger() {
-        return BulgogiBurger;
-    }
-    public int getChickenBurger() {
-        return ChickenBurger;
-    }
-    public int getFrenchFries() {
-        return FrenchFries;
-    }
-    public int getCheeseStick() {
-        return CheeseStick;
-    }
-    public int getChickenNuggets() {
-        return ChickenNuggets;
-    }
-    public int getOrangejuice() {
-        return Orangejuice;
-    }
-    public int getCocaCola() {
-        return CocaCola;
-    }
-    public int getSprite() {
-        return Sprite;
+    public int getSelectedNum(int k) {
+        if(k == 0)
+            return KrabbyPatty;
+        else if(k == 1)
+            return BulgogiBurger;
+        else if(k == 2)
+            return ChickenBurger;
+        else if(k == 3)
+            return FrenchFries;
+        else if(k == 4)
+            return CheeseStick;
+        else if(k == 5)
+            return ChickenNuggets;
+        else if(k == 6)
+            return Orangejuice;
+        else if(k == 7)
+            return CocaCola;
+        else
+            return Sprite;
     }
 
-    public boolean getCheckStateofKrabbyPatty() {
-        return inventoryStateofKrabbyPatty;
-    }
-    public boolean getCheckStateofBulgogiBurger() {
-        return inventoryStateofBulgogiBurger;
-    }
-    public boolean getCheckStateofChickenBurger() {
-        return inventoryStateofChickenBurger;
-    }
-    public boolean getCheckStateofFrenchFries() {
-        return inventoryStateofFrenchFries;
-    }
-    public boolean getCheckStateofCheeseStick() {
-        return inventoryStateofCheeseStick;
-    }
-    public boolean getCheckStateofChickenNuggets() {
-        return inventoryStateofChickenNuggets;
-    }
-    public boolean getCheckStateofOrangejuice() {
-        return inventoryStateofOrangejuice;
-    }
-    public boolean getCheckStateofCocaCola() {
-        return inventoryStateofCocaCola;
-    }
-    public boolean getCheckStateofSprite() {
-        return inventoryStateofSprite;
+    public boolean getCheckState(int k) {
+        if(k == 0)
+            return inventoryStateofKrabbyPatty;
+        else if(k == 1)
+            return inventoryStateofBulgogiBurger;
+        else if(k == 2)
+            return inventoryStateofChickenBurger;
+        else if(k == 3)
+            return inventoryStateofFrenchFries;
+        else if(k == 4)
+            return inventoryStateofCheeseStick;
+        else if(k == 5)
+            return inventoryStateofChickenNuggets;
+        else if(k == 6)
+            return inventoryStateofOrangejuice;
+        else if(k == 7)
+            return inventoryStateofCocaCola;
+        else
+            return inventoryStateofSprite;
     }
 
     public void setInventoryState(int KrabbyPatty, int BulgogiBurger, int ChickenBurger, int FrenchFries, int CheeseStick, int ChickenNuggets, int Orangejuice, int CocaCola, int Sprite) {
