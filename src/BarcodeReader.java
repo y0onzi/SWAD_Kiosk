9b10169ffa1a4f4a821e4605a1b1ff8220022ea7
y0onzi <20190290@sungshin.ac.kr>
2023-06-03 15:18:09 +0900

    Controller - processPayment() 수정 BarcodeReader/CardReader 수정 Receipt 추가

--- src/BarcodeReader.java
+++ src/BarcodeReader.java
@@ -1,29 +1,26 @@
 public class BarcodeReader {
-//    String CouponCode;
-//
-//    public BarcodeReader() {
-//    }
-//
-//    public String getCouponCode() {
-//        return CouponCode;
-//    }
-//
-//    public void readCoupon(String CouponCode){
-//        this.CouponCode = CouponCode;
-//    }
-//
-//    private void useCoupon(String CouponCode){
-//        //버튼말고 키보드로 직접 입력 받기
-//        if(CouponCode.equals("2023drinkfree")){
-//            System.out.println("음료수 무료 제공 쿠폰 적용완료!");
-//            //controller에서 가격조절필요
-//            //음료수 선택안했으면?
-//        }
-//        else if (CouponCode.equals("2023discount50")){
-//            System.out.println("주문 가격에서 50퍼센트 할인 적용완료!");
-//            //controller에서 가격조절필요
-//        }
-//
-//    }
+    String CouponCode;
+    Controller controller;
+
+    public BarcodeReader() {
+    }
+
+    public void readCoupon(String CouponCode){
+        this.CouponCode = CouponCode;
+        useCoupon(CouponCode);
+    }
+    private void useCoupon(String CouponCode){
+       /*if(CouponCode.equals("2023drinkfree")){
+            System.out.println("음료수 무료 제공 쿠폰 적용완료!");
+            //controller에서 가격조절필요
+            //음료수 선택안했으면? - 일단은 고려X
+        }
+        else*/ if (CouponCode.equals("2023discount50")){
+            System.out.println("주문 가격에서 50퍼센트 할인 적용완료!");
+            //controller에서 가격조절필요
+            System.out.println("최종 결제 금액은: "+ controller.applyCouponAndGetPrice());
+        }
+
+    }
 }
 
